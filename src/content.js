b097ee9a245f386707d14362ac893d35b6c46dfe
Trevyn Mace <tmace@1800contacts.com>
2019-09-03 09:55:34 -0600

    removing items on load, showing on click

--- src/content.js
+++ src/content.js
@@ -1,3 +1,15 @@
+chrome.storage.sync.get({
+	    elementsToRemove: ''
+  	}, function(items) {
+	    var elementsArray = items.elementsToRemove.split("\n");
+	    
+	    for (i = 0; i < elementsArray.length; i++) {
+	    	var className = '.' + elementsArray[i];
+	    	$(className).hide();
+		}
+	}
+);
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 	    if( request.message === "clicked_browser_action" ) {
@@ -8,7 +20,7 @@ chrome.runtime.onMessage.addListener(
 				    
 				    for (i = 0; i < elementsArray.length; i++) {
 				    	var className = '.' + elementsArray[i];
-				    	$(className).remove();
+				    	$(className).show();
 					}
 				}
 			);
