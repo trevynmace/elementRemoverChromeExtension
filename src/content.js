b92a81b219b5c85b09d6bcc2911a347964c1127f
Trevyn Mace <tmace@1800contacts.com>
2019-09-03 10:03:35 -0600

    limiting to just kanbanflow, WIP

--- src/content.js
+++ src/content.js
@@ -1,14 +1,16 @@
-chrome.storage.sync.get({
-	    elementsToRemove: ''
-  	}, function(items) {
-	    var elementsArray = items.elementsToRemove.split("\n");
-	    
-	    for (i = 0; i < elementsArray.length; i++) {
-	    	var className = '.' + elementsArray[i];
-	    	$(className).hide();
+if (window.location == "https://kanbanflow.com/board/w217sn") { //TODO: enable pattern matching. grab this from storage. add to config page.
+	chrome.storage.sync.get({
+		    elementsToRemove: ''
+	  	}, function(items) {
+		    var elementsArray = items.elementsToRemove.split("\n");
+		    
+		    for (i = 0; i < elementsArray.length; i++) {
+		    	var className = '.' + elementsArray[i];
+		    	$(className).hide();
+			}
 		}
-	}
-);
+	);
+}
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
